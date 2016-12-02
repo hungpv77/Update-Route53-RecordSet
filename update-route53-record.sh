6eb048afbd55f05ab8e7e8990445325f34de1f39
hungpv77 <hung@fabfitfun.com>
2016-12-02 11:01:42 +0700

    Debugging

--- update-route53-record.sh
+++ update-route53-record.sh
@@ -5,7 +5,7 @@ HOSTED_ZONE_ID="Z1986QIYBBYSUJ"
 main(){
     IP_LIST_UPDATE=$(updated_ip_list)
     echo "DEBUGGING: ip list updated: $IP_LIST_UPDATE"
-    update_route53_record ${IP_LIST_UPDATE}
+    update_route53_record $IP_LIST_UPDATE
 }
 
 # Get IP List of $DOMAIN_NAME from route53
