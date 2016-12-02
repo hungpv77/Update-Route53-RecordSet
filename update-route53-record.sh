c9e054e705246fe9add955f7214173133c664137
Ubuntu <ubuntu@ip-172-30-0-220.ec2.internal>
2016-12-02 03:47:11 +0000

    Debuggin

--- update-route53-record.sh
+++ update-route53-record.sh
@@ -3,22 +3,22 @@ DOMAIN_NAME="route53.fffdev.com"
 HOSTED_ZONE_ID="Z1986QIYBBYSUJ"
 
 main(){
-    IP_LIST_UPDATE=$(updated_ip_list)    
+    IP_LIST_UPDATE=$(updated_ip_list)
     update_route53_record ${IP_LIST_UPDATE}
 }
 
 # Get IP List of $DOMAIN_NAME from route53
-get_ip_list(){    
+get_ip_list(){
     RECORD_SET_JSON=$( aws route53 list-resource-record-sets --hosted-zone-id Z1986QIYBBYSUJ --query "ResourceRecordSets[?Name == '$DOMAIN_NAME.']")
 
-    #Remove the first and last character in string  to convert json array to json object  
+    #Remove the first and last character in string  to convert json array to json object
     RECORD_SET_JSON=${RECORD_SET_JSON:1:-1}
 
     # Need to install jq to parse json http://xmodulo.com/how-to-parse-json-string-via-command-line-on-linux.html
     # Get value of ResourceRecords
     RECORD_SET_JSON=$( echo $RECORD_SET_JSON | jq -r '.ResourceRecords' )
 
-    echo $RECORD_SET_JSON    
+    echo $RECORD_SET_JSON
 }
 
 updated_ip_list(){
@@ -29,18 +29,18 @@ updated_ip_list(){
     # Get IP list from Route53 by invoking get_ip_list
     IP_LIST=$(get_ip_list)
 
-    echo "DEBUG: IP_LIST: $IP_LIST"
+    echo "DEBUGGING: IP_LIST: $IP_LIST"
 
     # Get length of json array
-    LENGTH=$(echo $IP_LIST | jq '. | length')
+    #LENGTH=$(echo $IP_LIST | jq '. | length')
 
     # Add one element to last array
-    IP_LIST=$(echo $IP_LIST | jq '.['$LENGTH'].Value |= .+ '\"$IP\"'')
-        
-    echo $IP_LIST
+    #IP_LIST=$(echo $IP_LIST | jq '.['$LENGTH'].Value |= .+ "'$IP'"'
+
+    #echo $IP_LIST
 }
 
-update_route53_record(){   
+update_route53_record(){
     echo "IP_LIST_UPDATE: $1"
     JSON_REQUEST='{
               "Comment": "Delete the A record set",
