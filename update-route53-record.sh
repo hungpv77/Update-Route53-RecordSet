da6592a24a44912d4f256c34c2af3a31f5c2d6a7
hungpv77 <hung@fabfitfun.com>
2016-12-05 11:36:55 +0700

    Debugging

--- update-route53-record.sh
+++ update-route53-record.sh
@@ -4,8 +4,9 @@ DOMAIN_NAME="route53.fffdev.com"
 HOSTED_ZONE_ID="Z1986QIYBBYSUJ"
 
 main(){
-    UPDATED_IP_LIST=$(updated_ip_list)    
-    update_route53_record "$UPDATED_IP_LIST"
+    # UPDATED_IP_LIST=$(updated_ip_list)    
+    # update_route53_record "$UPDATED_IP_LIST"
+    updated_ip_list
 }
 
 # Get IP List of $DOMAIN_NAME from route53
@@ -27,29 +28,29 @@ updated_ip_list(){
     IP_LIST=$(get_ip_list)
     echo "DEBUGGING IP_LIST return from Route53: $IP_LIST" | tee /var/log/update-route53.log
 
-    # Get public IP of running instance
-    IP=$( curl http://169.254.169.254/latest/meta-data/public-ipv4 ) 
-    echo "DEBUGGING IP: $IP" >> /var/log/update-route53.log
+    # # Get public IP of running instance
+    # IP=$( curl http://169.254.169.254/latest/meta-data/public-ipv4 ) 
+    # echo "DEBUGGING IP: $IP" >> /var/log/update-route53.log
     
-    # Get length of json array
-    LENGTH=$(echo $IP_LIST | jq '. | length')
+    # # Get length of json array
+    # LENGTH=$(echo $IP_LIST | jq '. | length')
 
-    # while is length of IP is 0
-    # while [ -z "$IP" ]
-    # do
-    #     # sleep 5 second to wait network card drivers are loaded
-    #     sleep 5
-    #     IP=$( curl http://169.254.169.254/latest/meta-data/public-ipv4 )
-    #     echo "IP: $IP" >> /var/log/update-route53.log
+    # # while is length of IP is 0
+    # # while [ -z "$IP" ]
+    # # do
+    # #     # sleep 5 second to wait network card drivers are loaded
+    # #     sleep 5
+    # #     IP=$( curl http://169.254.169.254/latest/meta-data/public-ipv4 )
+    # #     echo "IP: $IP" >> /var/log/update-route53.log
 
-    # done
+    # # done
 
 
-    # Add one element to last array
-    IP_LIST=$(echo $IP_LIST | jq '.['$LENGTH'].Value |= .+ '\"$IP\"'')
-    echo "DEBUGGING IP_LIST after added one element: $IP_LIST" >> /var/log/update-route53.log
+    # # Add one element to last array
+    # IP_LIST=$(echo $IP_LIST | jq '.['$LENGTH'].Value |= .+ '\"$IP\"'')
+    # echo "DEBUGGING IP_LIST after added one element: $IP_LIST" >> /var/log/update-route53.log
 
-    echo $IP_LIST
+    # echo $IP_LIST
 }
 
 update_route53_record(){   
